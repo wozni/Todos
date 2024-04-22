69e4025e54b8ba6002f402320b4169a142f3d24c
Marcin <mwozniak@teldat.local>
2024-04-22 13:12:48 +0200

    Initial project structure

---
+++ src/Todos.Service/Program.cs
@@ -0,0 +1,32 @@
+﻿// Setup services
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddSqlServer<AppContext>(builder.Configuration.GetConnectionString("Default"));
+var app = builder.Build();
+app.MapTodosApi();
+app.Run();
+
+public class AppContext : DbContext
+{
+    public DbSet<ToDoList> TodoLists { get;set;}
+
+    public AppContext(DbContextOptions<AppContext> options): base(options)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<ToDoList>(todo => {
+            todo.ToTable("TodoLists");
+            todo.HasKey(x => x.Id);
+            todo.Property(x => x.Name).HasMaxLength(30);
+        });
+    }
+}
+
+public class ToDoList
+{
+    public int Id { get; set; }
+    public string Name { get;set; }
+    public string Description { get;set;}
+}
+
+
---
+++ src/Todos.Service/TodoListsApi.cs
@@ -0,0 +1,12 @@
+﻿namespace MAVLink.Gateway.Service;
+
+public static class TodoListsApi
+{
+    public static void MapTodosApi(this WebApplication app)
+    {
+        app.MapGet("/todos", async ([FromServices] AppContext context) =>
+        {
+            await context.TodoLists.ToListAsync();
+        });
+    }
+}
