a638b62c255bda43d8333c44940b01718c6943aa
Marcin <mwozniak@teldat.local>
2024-04-25 12:29:39 +0200

    Work in progress

---
+++ src/Todos.Service/DiagnosticsApi.cs
@@ -0,0 +1,14 @@
+﻿namespace MAVLink.Gateway.Service;
+
+public static class DiagnosticsApi
+{
+    public static void MapDiagnosticsApi(this WebApplication app)
+    {
+        app.MapGet("/status", (IConfiguration configuration) => 
+            Results.Ok(new
+        {
+            stamp = DateTime.UtcNow,
+            banner = configuration["WelcomeBanner"]
+        }));
+    }
+}
--- src/Todos.Service/Program.cs
+++ src/Todos.Service/Program.cs
@@ -2,4 +2,5 @@
 builder.Services.AddSqlServer<AppContext>(builder.Configuration.GetConnectionString("Default"));
 var app = builder.Build();
 app.MapTodosApi();
+app.MapDiagnosticsApi();
 app.Run();
